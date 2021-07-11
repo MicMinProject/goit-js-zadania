e910f5c7bf6f3a53802d07e2886352860570a3d1
MicMinProject <67431264+MicMinProject@users.noreply.github.com>
2021-07-11 22:34:59 +0200

    homework

---
+++ js/102-2.js
@@ -0,0 +1,12 @@
+function add(a, b, c){
+  return a+b+c;
+}
+
+let result = add(15, 27, 10);
+console.log(result);
+result = add(10, 20, 30);
+console.log(result);
+result = add(5, 10, 15);
+console.log(result);
+result = add(15312, 237, 11230);
+console.log(result);
